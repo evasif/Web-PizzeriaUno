c95d9760ed8a080eafeb44c190932df86032d051
evasif <evase16@ru.is>
2019-11-11 19:13:40 +0000

    PizzeriaUno ready

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return <h1>Welcom to my application</h1>
+};
+
+ReactDom.render(<App/>, document.getElementById('app'));
+
+
+
+//ReactDOM.render(<App />, document.getElementById('app'));
